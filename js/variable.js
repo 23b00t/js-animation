d3fc90d590bde210e109a7b5297e9a6bcea421bc
Daniel Kipp <daniel.kipp@gmail.com>
2025-01-14 12:57:35 +0100

    cleaning up

--- js/variable.js
+++
@@ -1,9 +0,0 @@
-export const container = document.getElementById('container');
-export const elem = document.getElementById('animate');
-export const btn = document.querySelector('#start');
-export let id = null;
-
-// Funktion zum Setzen von `id` (wird benötigt, um `id` zu aktualisieren)
-export const setId = (newId) => {
-  id = newId;
-};
